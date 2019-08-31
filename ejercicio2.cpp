6c26f4eb8d6d3b374579aaf1fc733562dcaf8bb5
oikawa18 <54276784+oikawa18@users.noreply.github.com>
2019-08-30 22:45:25 -0500

    Completado

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,23 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
 
 
-int moda(vector<int> &lista);
-vector<int> leerConsole();
+float calcularModa(vector<int> &lista);
+vector<int> leerConsola();
 
-vector<int> leerConsola() {
-    //Ingrese un numero positivo, escriba -1 si ya no quiere ingresar mas numeros:
+
+
+float calcularModa(vector<int> &lista) {
+    unordered_map<int, int> map;
+    for (int i = 0; i < lista.size(); i++) {
+        map[lista[i]]++;
+    }
+    int moda, max = -10;
+    for (auto &it : map) {
+        if (it.second > max) {
+            max = it.second;
+            moda = it.first;
+        }
+    }
+    return moda;
 }
 
-int moda(vector<int> &lista) {
-    /*
-    La moda es el elemento que mas se repite en la lista de elementos
-     */
-    return null;
+vector<int> leerConsola() {
+    vector<int> lista;
+    int num;
+    while (true) {
+        cout<<"Ingrese un numero positivo, escriba -1 si ya no quiere ingresar mas numeros: ";
+        cin>>num;
+        if (num<0) {
+            break;
+        }
+        lista.push_back(num);
+    }
+    return lista;
 }
 
 
 int main() {
-    cout<<"Calcular la moda de los numeros ingresados:\n\n";
-    vector<int> leer_usuario = leerConsole();
-    int moda_elemento = moda(leer_usuario);
-    cout<<"La moda es: "<<moda_elemento<<"\n";
+    cout<<"Calcular la moda de numeros ingresados:\n";
+    vector<int> leerUsuario = leerConsola();
+
+    int moda = calcularModa(leerUsuario);
+    cout<<"La moda es : "<<moda<<"\n";
+    return 0;
 }
