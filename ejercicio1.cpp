9ff0b822b44266d5ab8fa3ee41ee4ff6dcfe1735
oikawa18 <54276784+oikawa18@users.noreply.github.com>
2019-08-30 22:42:26 -0500

    Completado

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,23 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
 
-
-
-float promedio(vector<int> &lista);
+float calcularPromedio(vector<int> &v);
 vector<int> leerConsola();
 
-vector<int> leerConsola() {
-    //Ingrese un numero positivo, escriba -1 si ya no quiere ingresar mas numeros:
+float calcularPromedio(vector<int> &v) {
+    int suma = 0;
+    for (int i = 0; i < v.size(); i++) {
+        suma += v[i];
+    }
+    return (float)suma/v.size();
 }
 
-int promedio(vector<int> &lista) {
-    /*
-    El promedio es la suma de todos los elementos dividido entre el total de elementos
-     */
-    return null;
+
+
+vector<int> leerConsola() {
+    vector<int> lista;
+    int num;
+    while (true) {
+        cout<<"Ingrese un numero positivo, escriba -1 si ya no quiere ingresar mas numeros. ";
+        cin>>num;
+        if (num<0) {
+            break;
+        }
+        lista.push_back(num);
+    }
+    return lista;
 }
+
 
 int main() {
-    cout<<"Calcular el promedio de los ingresados:\n\n";
-    vector<int> leer_usuario = leerConsola();
-    float promedio_elemento = promedio(leer_usuario);
-    cout<<"El promedio es: "<<fixed<<setprecision(2)<<promedio_elemento<<"\n";
+    cout<<"Calcular el promedio de numeros ingresados:\n";
+    vector<int> leerUsuario = leerConsola();
+    float promedio = calcularPromedio(leerUsuario);
+    cout<<"El promedio es: "<<promedio<<"\n";
+    
+    return 0;
 }
